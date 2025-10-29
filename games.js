ddecea7547bcb831505a4a59beab597e45a79a41
Cosmos <soycosmosgg@gmail.com>
2025-10-29 13:26:21 +0100

    Update games.js

--- games.js
+++ games.js
@@ -142,12 +142,13 @@ const juegos = [
   { name: "Canon Blast", url: "https://bitlifeonline.github.io/cannon-blast/", img: "assets/games/canon-blast.webp", tags: ["arcade", "skill"] },
   { name: "Cubes 2048.io", url: "https://bitlifeonline.github.io/cubes-2048-io/", img: "assets/games/cubes-2048.io.webp", tags: ["arcade", "skill"] },
   { name: "Obby parkour ultimate", url: "https://bitlifeonline.github.io/obby-parkour-ultimate/", img: "assets/games/obby-parkour-ultimate.webp", tags: ["platform", "skill"] },
-  { name: "Stickmerge", url: "https://webglmath.github.io/stickmerge/", img: "assets/games/stickmerge.webp", tags: ["action", "skill", "merge"] }
+  { name: "Stickmerge", url: "https://webglmath.github.io/stickmerge/", img: "assets/games/stickmerge.webp", tags: ["action", "skill", "merge"] },
   { name: "Squad busters", url: "https://web.cloudmoonapp.com/es/game/com.supercell.squad/?from=list", img: "assets/games/squad-busters.webp", tags: ["action", "skill", "merge"] }
 
 
 ];
   
+
 
 
 
