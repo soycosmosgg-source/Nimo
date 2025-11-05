1ecf5a2f1854cfb067a506e3340c0b1d63ff4674
Cosmos <soycosmosgg@gmail.com>
2025-11-05 16:02:59 +0100

    Update games.js

--- games.js
+++ games.js
@@ -155,10 +155,11 @@ const juegos = [
   { name: "Uphill rush 3", url: "https://files.silvergames.com/flash/ruffle/player.php?id=886", img: "assets/games/uphill-rush.webp", tags: ["racing"] },
   { name: "Uphill rush 6", url: "https://frivez.com/flashgame?load=uphill-rush-6", img: "assets/games/uphill-rush-6.webp", tags: ["racing"] },
   { name: "Happy Wheels", url: "https://flash-games.io/happy-wheels.embed", img: "assets/games/happy-wheels.webp", tags: ["racing"] },
-  { name: "Apple worm", url: "https://apple-worm.github.io/apple-worm/", img: "assets/games/apple-worm.webp", tags: ["puzzle"] },
+  { name: "Apple worm", url: "https://www.coolmathgames.com/0-apple-worm/play", img: "assets/games/apple-worm.webp", tags: ["puzzle"] },
   { name: "Super smash flash 2", url: "https://flash-games.io/super-smash-flash-2.embed", img: "assets/games/super-smash-flash-2.webp", tags: ["action"] }
   
 
 
 ];
+
 
