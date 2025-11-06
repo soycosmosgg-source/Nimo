bc39203d77269e73d85a83276de5eedef07011f1
Cosmos <soycosmosgg@gmail.com>
2025-11-06 18:54:22 +0100

    Update games.js

--- games.js
+++ games.js
@@ -161,11 +161,13 @@ const juegos = [
   { name: "Uphill rush 6", url: "https://frivez.com/flashgame?load=uphill-rush-6", img: "assets/games/uphill-rush-6.webp", tags: ["racing"] },
   { name: "Happy Wheels", url: "https://flash-games.io/happy-wheels.embed", img: "assets/games/happy-wheels.webp", tags: ["racing"] },
   { name: "Kindergarten", url: "https://2games.io/game/kindergarten/", img: "assets/games/kindergarten.webp", tags: ["horror"] },
-  { name: "Super smash flash 2", url: "https://flash-games.io/super-smash-flash-2.embed", img: "assets/games/super-smash-flash-2.webp", tags: ["action"] }
+  { name: "Super smash flash 2", url: "https://flash-games.io/super-smash-flash-2.embed", img: "assets/games/super-smash-flash-2.webp", tags: ["action"] },
+  { name: "Gun runner", url: "./source-games/gun-runner.html", img: "assets/games/gun-runner.webp", tags: ["action"] }
   
 
 
 ];
+
 
 
 
