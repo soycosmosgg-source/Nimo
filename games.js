1edff1c82f7e9620269b957dde6005ec9f07841b
Cosmos <soycosmosgg@gmail.com>
2025-11-07 14:50:35 +0100

    updated

--- games.js
+++ games.js
@@ -160,8 +160,11 @@ const juegos = [
   { name: "Uphill rush 3", url: "https://files.silvergames.com/flash/ruffle/player.php?id=886", img: "assets/games/uphill-rush.webp", tags: ["racing"] },
   { name: "Uphill rush 6", url: "https://frivez.com/flashgame?load=uphill-rush-6", img: "assets/games/uphill-rush-6.webp", tags: ["racing"] },
   { name: "Happy Wheels", url: "https://flash-games.io/happy-wheels.embed", img: "assets/games/happy-wheels.webp", tags: ["racing"] },
-  { name: "Kindergarten", url: "https://2games.io/game/kindergarten/", img: "assets/games/kindergarten.webp", tags: ["horror"] },
-  { name: "Super smash flash 2", url: "https://flash-games.io/super-smash-flash-2.embed", img: "assets/games/super-smash-flash-2.webp", tags: ["action"] }
+  { name: "Kindergarten", url: "./source-games/kindergarten-1.html", img: "assets/games/kindergarten.webp", tags: ["horror"] },
+  { name: "Kindergarten 2", url: "./source-games/kindergarten-2.html", img: "assets/games/kindergarten-2.webp", tags: ["horror"] },
+  { name: "Kindergarten 3", url: "./source-games/kindergarten-3.html", img: "assets/games/kindergarten-2.webp", tags: ["horror"] },
+  { name: "Super smash flash 2", url: "https://flash-games.io/super-smash-flash-2.embed", img: "assets/games/super-smash-flash-2.webp", tags: ["action"] },
+  { name: "Gun runner", url: "./source-games/gun-runner.html", img: "assets/games/gun-runner.webp", tags: ["action"] },
   
 
 
