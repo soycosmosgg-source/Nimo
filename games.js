d77f8ea28c25d371f0403b2cd174e4e6d6b9dbc8
Cosmos <soycosmosgg@gmail.com>
2025-11-07 15:17:48 +0100

    updated

--- games.js
+++ games.js
@@ -165,7 +165,7 @@ const juegos = [
   { name: "Super smash flash 2", url: "https://flash-game-images.io/super-smash-flash-2.embed", img: "assets/game-images/super-smash-flash-2.webp", tags: ["action"] },
   { name: "Gun runner", url: "./games/gun-runner.html", img: "assets/game-images/gun-runner.webp", tags: ["action"] },
   { name: "Candy Crush", url: "./games/candy-crush.html", img: "assets/game-images/candy-crush.webp", tags: ["puzzle"] },
-  { name: "Crazy Cars", url: "./games/crazy-cars.html", img: "assets/game-images/crazy-cars.webp", tags: ["racing"] },
+  { name: "Crazy Cars", url: "./games/crazy-cars.html", img: "assets/game-images/crazy-cars.webp", tags: ["racing", "2players"] },
   { name: "Build now.gg", url: "./games/buildnow.gg.html", img: "assets/game-images/buildnow.gg.webp", tags: ["action"] },
   
 
