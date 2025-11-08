61ca73258d5f3402cbedeab9b158c7929337c6db
Cosmos <soycosmosgg@gmail.com>
2025-11-08 12:57:55 +0100

    Added Amanda the adventurer

--- games.js
+++ games.js
@@ -81,6 +81,7 @@ const juegos = [
   { name: "Buckshot Roulette", url: "./games/buckshot-roulette.html", img: "assets/game-images/buckshot-roulette.webp", tags: ["platform"] },
   { name: "Cuphead", url: "./games/cuphead.html", img: "assets/game-images/cuphead.webp", tags: ["platform", "skill", "action"] },
   { name: "R.E.P.O", url: "./games/R.E.P.O.html", img: "assets/game-images/repo.webp", tags: ["horror", "action"] },
+  { name: "Amanda the adventurer", url: "./games/amanda-the-adventurer.html", img: "assets/game-images/amanda-the-adventurer.webp", tags: ["horror"] },
   { name: "Bad Parenting 1", url: "./games/bad-parenting.html", img: "assets/game-images/bad-parenting.webp", tags: ["horror"] },
   { name: "Fears to fathom home alone", url: "https://gnhustgames.github.io/class-69/fearstofathom-homealone/", img: "assets/game-images/ftp-home-alone.webp", tags: ["horror"] },
   { name: "Gta Chinatown", url: "https://retrogamesonline.io/grand-theft-auto-chinatown-wars-usa-enfrdeesit.embed", img: "assets/game-images/gta-chinatown.webp", tags: ["action", "skill"] },
