75a3c3916c75e7d9e1ec4d9c2003c50ba8f9d696
Cosmos <soycosmosgg@gmail.com>
2025-11-08 13:13:22 +0100

    Update sitemap.xml.js

--- functions/sitemap.xml.js
+++ functions/sitemap.xml.js
@@ -1,21 +1,23 @@
 export async function onRequest() {
-  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+  const today = new Date().toISOString();
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>\n
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
     <loc>https://nimogames.pages.dev/</loc>
-    <lastmod>2025-10-31T12:09:55+00:00</lastmod>
+    <lastmod>${today}</lastmod>
     <changefreq>daily</changefreq>
     <priority>1.0</priority>
   </url>
   <url>
     <loc>https://nimogames.pages.dev/privacy.html</loc>
-    <lastmod>2025-10-27T13:01:50+00:00</lastmod>
+    <lastmod>${today}</lastmod>
     <changefreq>monthly</changefreq>
     <priority>0.3</priority>
   </url>
   <url>
     <loc>https://nimogames.pages.dev/terms.html</loc>
-    <lastmod>2025-10-27T13:01:50+00:00</lastmod>
+    <lastmod>${today}</lastmod>
     <changefreq>monthly</changefreq>
     <priority>0.3</priority>
   </url>
